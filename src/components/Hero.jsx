2afd9387e85f0bcae56f2e484b5172b90be5be9f
powel ikechukwu <ikechukwupowei21@gmail.com>
2023-03-17 22:31:45 +0100

    refractored the image in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import HeroImage from '../assets/images/heroImage.jpg'
 
 const Hero = () => {
   return (
-    <div className='w-full  pt-[5em] flex flex-col px-10 items-center  '>
+    <div className='w-full  pt-[5em] flex flex-col px-5 items-center md:max-w-[1400px] '>
         <div className=''> 
             <div className='lg:pl-[17em]'>
                 <p className='text-[#2D9CDB] text-[.8rem] pb-2 '>Unhappy   with your website?</p>
@@ -15,7 +15,7 @@ const Hero = () => {
 
             </div>
             
-            <img className='pb-5 object-cover w-ful h-full   rounded' src={HeroImage} alt="hero" />
+            <img className='pb-5 object-cover w-full  block rounded' src={HeroImage} alt="hero" />
         </div>
 
         <div  className='lg:pl-[17em]'>
@@ -26,12 +26,12 @@ const Hero = () => {
              
           <p className='text-[.8rem] tracking-wider font-[400] py-5 lg:w-[40%]'>We help transform your ideas into real world applications that will outperform your toughest competition and help you achieve your strategic goals in short period of time.</p>
 
-          <div>
+          <div className='max-w-[400px] '>
               <form action="">
                   <label htmlFor="email" className='text-[.8rem]'>Want us to contact you?</label><br />
                   <div className='flex relative'>
-                    <input className=' rounded-[.8rem]  border  p-2 px-7 mt-2 bg-[#F2F2F2]' type="email" id='email' placeholder='Email' />
-                    <button className='absolute ml-[14em] rounded-[.8em] mt-2 '>Join</button>
+                    <input className=' rounded-[.8rem] w-[15em]  border  p-2 px-7 mt-2 bg-[#F2F2F2]' type="email" id='email' placeholder='Email' />
+                    <button className=' rounded-[.5em] mt-2 '>Join</button>
                   </div>
               </form> 
           </div>
